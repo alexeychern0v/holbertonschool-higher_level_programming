3340cdc0f38487638ed68f4ca3bc78b36533618e
Alexey Chernov <alexchernov063@gmail.com>
2024-01-26 08:19:08 -0800

    changed code

--- python-if_else_loops_functions/6-print_comb3.py
+++ python-if_else_loops_functions/6-print_comb3.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-for i in range(10):
-    for j in range(10):
-        if (i != j and i < j) and i <  9:
-            if (i == 8 and j == 9):
-                print("{0}{1}".format(i, j))
-            else:
-                print("{0}{1}, ".format(i, j), end="")
+for first in range(0, 9):
+    for second in range(first + 1, 10):
+        if (first != 8 or second != 9):
+            print("{}{}".format(first, second), end=", ")
+        else:
+            print("{}{}".format(first, second))
