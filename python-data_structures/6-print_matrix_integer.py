7cefac3a47393937db44cf077ba3209df61ec214
Alexey Chernov <alexchernov063@gmail.com>
2024-01-31 13:58:40 -0800

    matrix

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 def print_matrix_integer(matrix=[[]]):
-    for row in :wqmatrix:
+    for row in matrix:
         for i in range(len(row)):
             print("{:d}".format(row[i]), end=" " if i < len(row) - 1 else "")
         print()
