caea24baa26a1e95132e01e20390d847d68ed6ad
Alexey Chernov <97944947+alexeychern0v@users.noreply.github.com>
2024-02-19 21:46:54 +0000

    task 3

---
+++ python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Returns True if the object is an instance
+"""
+
+
+def is_kind_of_class(obj, a_class):
+    """
+    Checks inheritance
+    """
+
+    if isinstance(obj, a_class):
+
+        return True
+    return False
