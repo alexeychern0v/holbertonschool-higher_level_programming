55d5394df6ce4f41bd195475046e5dc1e52bee6d
Alexey Chernov <97944947+alexeychern0v@users.noreply.github.com>
2024-02-19 22:00:59 +0000

    edit comments

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
-"""
-Defines inherits_from
-"""
+"""Defines inherits_from"""
 
 
 def inherits_from(obj, a_class):
-    """
-    Returns True if object is an instance of a class that 
-    inherited (directly or indirectly) from the specified class
-    """
+    """Returns True if object is an instance of a class that 
+    inherited (directly or indirectly) from the specified class"""
 
     return isinstance(obj, a_class) and type(obj) is not a_class
