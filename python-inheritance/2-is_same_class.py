c1e88c1214115f7b040da6003dd1195702086a0e
Alexey Chernov <97944947+alexeychern0v@users.noreply.github.com>
2024-02-19 21:43:05 +0000

    delete ()

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -9,4 +9,4 @@ def is_same_class(obj, a_class):
     Checks if the object is exactly an 
     instance of the specified class
     """
-    return (type(obj) is a_class)
+    return type(obj) is a_class
