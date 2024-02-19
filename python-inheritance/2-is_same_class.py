077f48a7dfd0019b6265de58b4d2ae5f8d362791
Alexey Chernov <97944947+alexeychern0v@users.noreply.github.com>
2024-02-19 21:41:45 +0000

    2 task

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -6,6 +6,7 @@ Define the function is_same_class
 
 def is_same_class(obj, a_class):
     """
-    Checks if the object is exactly an instance of the specified class
+    Checks if the object is exactly an 
+    instance of the specified class
     """
-    return isinstance(obj, a_class)
+    return (type(obj) is a_class)
