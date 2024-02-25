c8d19ead46c59f5d2f4c1034ac47aeceac58123c
Alexey Chernov <97944947+alexeychern0v@users.noreply.github.com>
2024-02-25 16:04:30 +0000

    added update

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -100,3 +100,19 @@ class Rectangle(Base):
         """Returns [Rectangle] (<id>) <x>/<y> - <width>/<height>"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(
             self.id, self.__x, self.__y, self.__width, self.__height)
+        
+    def update(self, *args, **kwargs):
+        """Assigns an argument to each attribute"""
+        if args:
+            if len(args) > 0:
+                self.id = args[0]
+            if len(args) > 1:
+                self.__width = args[1]
+            if len(args) > 2:
+                self.__height = args[2]
+            if len(args) > 3:
+                self.__x = args[3]
+            if len(args) > 4:
+                self.__y = args[4]
+                
+    
