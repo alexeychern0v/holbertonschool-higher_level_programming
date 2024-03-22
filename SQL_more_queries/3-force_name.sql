5a212bbd5cb8e53fc4653754e4378226d8a1fe02
Alexey Chernov <alexchernov063@gmail.com>
2024-03-22 15:42:20 +0100

    added space

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,5 @@
 -- creates the table force_name on your MySQL server
-CREATE DATABASE IF NOT EXISTS force_name(
+CREATE DATABASE IF NOT EXISTS force_name (
     id INT,
     name VARCHAR(256) NOT NULL
 );
